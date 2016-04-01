f2c166803cec312564b6d07fff952a0a7efbc635
SCDevGit2 <arthur.lee@tylertech.com>
2016-04-01 15:14:06 -0400

    Somehow we excluded DatabaseLocation POCO

---
+++ src/SoftCode.BookmarkGenerator.Common/DTO/DatabaseLocation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftCode.BookmarkGenerator.Common.DTO
+{
+    public class DatabaseLocation
+    {
+        [Required, MaxLength(30)]
+        public string ServerName { get; set; }
+
+        [Required, MaxLength(30)]
+        public string DbName { get; set; }
+    }
+}
