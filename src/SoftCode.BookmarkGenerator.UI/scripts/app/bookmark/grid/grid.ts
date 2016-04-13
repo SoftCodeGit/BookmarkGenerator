785d47f69d74227f92938236bc0b78cb4d83c436
Mark Test <mjfschool@yahoo.com>
2016-04-13 16:09:33 -0400

    add grid to folder

---
+++ src/SoftCode.BookmarkGenerator.UI/scripts/app/bookmark/grid/grid.ts
@@ -0,0 +1,35 @@
+﻿import {Component, Input, OnInit, Output, EventEmitter} from 'angular2/core';
+import {Column} from './column';
+import {Sorter} from './sorter';
+
+@Component({
+    selector: 'grid',
+    templateUrl: './app/grid/grid-ui.html'
+})
+
+export class Grid implements OnInit {
+    @Input() rows: Array<any>;
+    @Input() columns: Array<any>;
+    @Input() name: string;
+
+    @Output() rowClicked = new EventEmitter();
+    @Output() viewClicked = new EventEmitter();
+
+    sorter = new Sorter();
+
+    sort(key) {
+        this.sorter.sort(key, this.rows);
+    }
+
+    ngOnInit() {
+        console.log(this.name);
+    }
+
+    onRowClicked(row: any) {
+        this.rowClicked.emit(row);
+    }
+
+    onViewClicked(row: any) {
+        this.viewClicked.emit(row);
+    }
+}
