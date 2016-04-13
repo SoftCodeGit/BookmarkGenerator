714f218e881313b534b9e5cf8d7855a83b328c18
Mark Test <mjfschool@yahoo.com>
2016-04-13 17:37:23 -0400

    option value added

---
+++ src/SoftCode.BookmarkGenerator.UI/scripts/app/bookmark/bookmark-option-value.ts
@@ -0,0 +1,18 @@
+﻿export interface IBookmarkOptionValue {
+    bookmarkCode: string;
+    formValues: BookmarkOptionValuePair[];
+}
+
+export class BookmarkOptionValue implements IBookmarkOptionValue {
+    bookmarkCode: string;
+    formValues: BookmarkOptionValuePair[];
+
+    constructor() {
+        this.formValues = [];
+    }
+}
+
+export class BookmarkOptionValuePair {
+    key: string;
+    value: string;
+}
