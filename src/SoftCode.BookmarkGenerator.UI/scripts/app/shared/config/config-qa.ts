e7aaa35f51ae408119e7c219a1948fcdb2de8f32
SCDevGit2 <arthur.lee@tylertech.com>
2016-04-26 07:43:09 -0400

    Added environment configuration infrustructure

---
+++ src/SoftCode.BookmarkGenerator.UI/scripts/app/shared/config/config-qa.ts
@@ -0,0 +1,9 @@
+﻿//TO-DO: use the actual domain name
+var domain = "bookmark-qa";
+export let CONFIG = {
+    baseUrls: {
+        bookmarkOptions: `http://${domain}/api/bookmark/BookmarkOptions/`,
+        bookmark: `http://${domain}/api/Bookmark/`
+    },
+    useMock: false
+}
